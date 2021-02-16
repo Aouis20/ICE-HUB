da43a8166b8a0b650a3770e0feaf73fb3d4cc2d9
Aouis20 <78351892+Aouis20@users.noreply.github.com>
2021-02-16 14:48:03 +0100

    menu-slider

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-function salut () {
-    console.log("brou bra bri")
+function openSlideMenu() {
+    document.getElementById("menu").style.width="250px"
+    document.getElementById("content").style.marginLeft = "250px"
+    document.getElementById("burgermenu").style.visibility = "hidden"
+}
+
+function closeSlideMenu() {
+    document.getElementById("menu").style.width="0"
+    document.getElementById("content").style.marginLeft = "0"
+    document.getElementById("burgermenu").style.visibility = "visible"
 }
